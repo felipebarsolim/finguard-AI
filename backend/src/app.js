f38d5ff27b09d0e60ef835abd2155561a0a02151
felipebarsolim <felipebarbosa30802@gmail.com>
2026-03-08 16:10:42 -0300

    feat: database connection interface with pool query proxy

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,17 +3,22 @@ import express from "express";
 import cors from "cors";
 
 import router from "./routes/router.js";
+import { database } from "./config/databaseConnection.js";
 
 const app = express();
 
 const port = process.env.PORT || 8081;
 
-const startServer = () => {
+const startServer = async () => {
     try {
         app.use(cors);
         app.use(express.json());
         app.use(cors);
         app.use("/", router);
+
+        const connectionDatabase = await database.query("SELECT 1");
+        if (connectionDatabase) console.log("Database connected");
+        else console.error("ERROR in database Connection");
 
         app.listen(port, () => {
             console.log(`Server online in ${port}`);
---
+++ backend/src/config/databaseConnection.js
@@ -0,0 +1,9 @@
+import { Pool } from "pg";
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+export const database = {
+    query: (text, params) => pool.query(text, params),
+};
